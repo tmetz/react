02bd2ba2784e7843be475eebd60419283280dc58
Tammy Metz <tmetz@github.com>
2020-04-20 14:43:49 -0400

    Updates

---
+++ precache-manifest.16fa0813597997dfc2051bf3fd3f8641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a733fbdce5eb14a1ba6fabf3f4a02d14",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6635cd65a7df2f28d77d",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c99e66ac96c3023672b9",
+    "url": "/react/static/js/2.116f85d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.116f85d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6635cd65a7df2f28d77d",
+    "url": "/react/static/js/main.cba73f0d.chunk.js"
+  },
+  {
+    "revision": "eb527aed6890176a2063",
+    "url": "/react/static/js/runtime-main.bcee33b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
